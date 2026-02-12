3605a7936d5af8c39dc51fb187a70c2257efcd09
view147 <teterryui123@gmail.com>
2026-02-12 13:20:04 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,22 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  // ตัวอย่างล็อกอินปลอมก่อน
+  if (user === "admin" && pass === "1234") {
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "main.html";
+  } else {
+    alert("Username หรือ Password ผิด");
+  }
+}
+
+function goRegister() {
+  window.location.href = "register.html";
+}
+
+function logout() {
+  localStorage.removeItem("loggedIn");
+  window.location.href = "login.html";
+}
 
