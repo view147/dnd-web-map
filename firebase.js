0c8471f11d42f711fce423fbc6e442ea95ac4087
view147 <teterryui123@gmail.com>
2026-03-07 19:31:51 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+
+const firebaseConfig = {
+
+apiKey: "YOUR_KEY",
+
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+projectId: "YOUR_PROJECT",
+
+storageBucket: "YOUR_PROJECT.appspot.com",
+
+messagingSenderId: "XXXX",
+
+appId: "XXXX"
+
+}
+
+initializeApp(firebaseConfig)
