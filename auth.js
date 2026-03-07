6ade2d68d86ea282cc784ad0a1e4ca4c56dccfd9
view147 <teterryui123@gmail.com>
2026-03-07 19:32:05 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,119 +1,19 @@
-/* =====================
-   GENERATE PLAYER CODE
-===================== */
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut }
 
-function generateCode(){
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 
-  const chars="ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+const auth = getAuth()
 
-  let code="PALE-";
+const provider = new GoogleAuthProvider()
 
-  for(let i=0;i<6;i++){
+document.getElementById("loginBtn").onclick = () => {
 
-    code+=chars[Math.floor(Math.random()*chars.length)]
-
-  }
-
-  return code
-}
-
-/* =====================
-   REGISTER
-===================== */
-
-window.register=function(){
-
-  const code=generateCode()
-
-  localStorage.setItem("playerCode",code)
-
-  localStorage.setItem("loggedIn","true")
-
-  alert(
-  "Player Created!\n\n"+
-  "Your Player Code:\n"+
-  code+
-  "\n\nSave this code!"
-  )
-
-  showGame()
+signInWithPopup(auth, provider)
 
 }
 
+document.getElementById("logoutBtn").onclick = () => {
 
-/* =====================
-   LOGIN
-===================== */
-
-window.login=function(){
-
-  const input=document.getElementById("playerCode").value.trim()
-
-  const saved=localStorage.getItem("playerCode")
-
-  const msg=document.getElementById("msg")
-
-  if(!saved){
-
-    msg.textContent="❌ No player found. Create one first."
-
-    return
-  }
-
-  if(input===saved){
-
-    localStorage.setItem("loggedIn","true")
-
-    showGame()
-
-  }else{
-
-    msg.textContent="❌ Invalid Player Code"
-
-  }
-
-}
-
-
-/* =====================
-   LOGOUT
-===================== */
-
-window.logout=function(){
-
-  localStorage.removeItem("loggedIn")
-
-  location.reload()
-
-}
-
-
-/* =====================
-   SHOW GAME
-===================== */
-
-function showGame(){
-
-  document.getElementById("loginPage").style.display="none"
-
-  document.getElementById("gamePage").style.display="block"
-
-  document.getElementById("playerId").textContent=
-  localStorage.getItem("playerCode")
-
-}
-
-
-/* =====================
-   AUTO LOGIN
-===================== */
-
-window.onload=function(){
-
-  if(localStorage.getItem("loggedIn")==="true"){
-
-    showGame()
-
-  }
+signOut(auth)
 
 }
