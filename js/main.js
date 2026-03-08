d00e407dc2879b1c80fcae1ee708ae96a835c40e
view147 <teterryui123@gmail.com>
2026-03-08 09:54:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log("DND WEB MAP LOADED")
+
+/* PAGE LOADED */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+console.log("Page ready")
+
+initStartButton()
+
+})
+
+/* START GAME BUTTON */
+
+function initStartButton(){
+
+const startBtn = document.querySelector(".start-btn")
+
+if(!startBtn) return
+
+startBtn.addEventListener("click", () => {
+
+console.log("Start Game clicked")
+
+// ไปหน้า map
+window.location.href = "pages/map.html"
+
+})
+
+}
