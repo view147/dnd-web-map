4403e3afc27c72e7896556a3b81e2167ea09eb55
view147 <teterryui123@gmail.com>
2026-03-08 09:34:04 +0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 
 const firebaseConfig = {
 
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXXX",
-appId: "XXXXX"
+apiKey: "AIzaSyD_ELoo4XEXn9Xoo7zulw7yG78yAsvDoEU",
+authDomain: "dnd-pale-character-5ec73.firebaseapp.com",
+projectId: "dnd-pale-character-5ec73",
+storageBucket: "dnd-pale-character-5ec73.firebasestorage.app",
+messagingSenderId: "514277998310",
+appId: "1:514277998310:web:35ca063d3021cc70b9c527"
 
 }
 
