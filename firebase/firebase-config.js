e392452309a0e0508ed4790fe82770be0e3b6f4b
view147 <teterryui123@gmail.com>
2026-03-08 09:55:23 +0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyD_ELoo4XEXn9Xoo7zulw7yG78yAsvDoEU",
-authDomain: "dnd-pale-character-5ec73.firebaseapp.com",
-projectId: "dnd-pale-character-5ec73",
-storageBucket: "dnd-pale-character-5ec73.firebasestorage.app",
-messagingSenderId: "514277998310",
-appId: "1:514277998310:web:35ca063d3021cc70b9c527"
-
+  apiKey: "AIzaSyD_ELoo4XEXn9Xoo7zulw7yG78yAsvDoEU",
+  authDomain: "dnd-pale-character-5ec73.firebaseapp.com",
+  projectId: "dnd-pale-character-5ec73",
+  storageBucket: "dnd-pale-character-5ec73.firebasestorage.app",
+  messagingSenderId: "514277998310",
+  appId: "1:514277998310:web:35ca063d3021cc70b9c527"
 }
 
 export const app = initializeApp(firebaseConfig)
+
+/* AUTH */
+export const auth = getAuth(app)
