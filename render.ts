a1c934d956e893bb39f5b210a8e70dce568120ab
view147 <teterryui123@gmail.com>
2026-06-15 18:07:35 +0700

    Update render.ts

--- render.ts
+++ render.ts
@@ -1,1 +1,15 @@
+import { GameState } from "../engine/game_state";
 
+export function render(state: GameState) {
+  console.clear();
+  console.log("PHASE:", state.turn.phase);
+  console.log("TURN:", state.turn.currentTurn);
+
+  state.players.forEach(p => {
+    console.log(
+      `${p.name} | HP ${p.hp}/${p.maxHp} | INF ${p.infectionStage}`
+    );
+  });
+
+  console.log("LOCATION:", state.world.locationId);
+}
