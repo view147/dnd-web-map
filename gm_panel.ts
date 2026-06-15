d5542e735138ec346dbd713e14ea7eac58b1b2ed
view147 <teterryui123@gmail.com>
2026-06-15 18:08:03 +0700

    Update gm_panel.ts

--- gm_panel.ts
+++ gm_panel.ts
@@ -1,1 +1,10 @@
+import { GameState } from "../engine/game_state";
+import { TurnManager } from "../engine/turn_manager";
 
+export function gmSetPhase(state: GameState, phase: any) {
+  TurnManager.setPhase(state, phase);
+}
+
+export function gmNextTurn(state: GameState) {
+  TurnManager.nextTurn(state);
+}
