41ac8a6e5c3b88d29c16f7a7b26a8b92064788b7
view147 <teterryui123@gmail.com>
2026-06-15 18:04:20 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,9 +4,3 @@ initGame({
   season: "season_1",
   players: 3
 })
-import { GameState } from "../engine/game_state";
-import { render } from "./render";
-
-export function mountUI(state: GameState) {
-  render(state);
-}
