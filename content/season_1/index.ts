240d96e8b2ef6fb41ab2ebed9ca6a6d764b9c1b2
view147 <teterryui123@gmail.com>
2026-06-16 10:16:30 +0700

    Create index.ts

---
+++ content/season_1/index.ts
@@ -0,0 +1,13 @@
+import { intro } from "./intro";
+import { maps } from "./maps";
+import { events } from "./events";
+import { endings } from "./endings";
+
+export const Season1 = {
+  id: "season_1",
+  name: "Ashes Before Dawn",
+  intro,
+  maps,
+  events,
+  endings,
+};
